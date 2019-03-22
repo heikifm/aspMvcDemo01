1330fe3c7b2a7dad3f13e091aa57f226c8c0f117
Singha Winter <heikifm@gmail.com>
2019-03-22 13:48:48 +0900

    first commit

---
+++ demo01/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hello world!";
+        }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
