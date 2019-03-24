69348fb698e0a887d13099526f8942146bcb2bab
Singha Winter <heikifm@gmail.com>
2019-03-24 13:16:53 +0900

    ASP.NET MVC5 프레임워크를 이용한 웹개발 Part.1 (기초1) Lesson1~10

--- demo01/Controllers/HomeController.cs
+++ demo01/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using demo01.Models;
 
 namespace demo01.Controllers
 {
@@ -10,8 +11,34 @@ namespace demo01.Controllers
     {
         public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "morning!" : "afternoon!";
             return View();
         }
+
+        [HttpGet]
+        public ViewResult TestForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult TestForm(MemberResponse memberResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("demoView", memberResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+
+
+
         // GET: Home
         //public string Index()
         //{
---
+++ demo01/Models/MemberResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace demo01.Models
+{
+    public class MemberResponse
+    {
+        [Required(ErrorMessage = "Input your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Input your email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Input your Tel")]
+        public string Tel { get; set; }
+
+        [Required(ErrorMessage = "Select attendance")]
+        public bool? Attend { get; set; }
+    }
+}
